9f0eba81ab77316a4f4cccd073ca82e864125d2a
Jordan Castelloe <jordan.castelloe@gmail.com>
2017-11-17 14:13:26 -0600

    started adding product description and images

--- scripts/vanilla.js
+++ scripts/vanilla.js
@@ -36,15 +36,43 @@
     }
     
 
-    products[0] = new product("Frozen Margarita 0", "Description", "9.99", "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/cl/13/05/frozen-margarita-cl-x.jpg?itok=4LuenRaw");
-    products[1] = new product("Frozen Margarita 1", "Description", "9.99", "url");
-    products[2] = new product("Frozen Margarita 2", "Description", "9.99", "url");
-    products[3] = new product("Frozen Margarita 3", "Description", "9.99", "url");
-    products[4] = new product("Frozen Margarita 4", "Description", "9.99", "url");
-    products[5] = new product("Frozen Margarita 5", "Description", "9.99", "url");
-    products[6] = new product("Frozen Margarita 6", "Description", "9.99", "url");
-    products[7] = new product("Frozen Margarita 7", "Description", "9.99", "url");
-    products[8] = new product("Frozen Margarita 8", "Description", "9.99", "url");
+    products[0] = new product(
+                "Frozen Margarita", 
+                "Margaritas are synonymous with Mexican food and, of course, Cinco de Mayo! Jack’s Ultimate Frozen Margaritas are a luscious frozen cocktail that will make you want to shout olé!", 
+                "7.99", 
+                "images/frozen-marg.jpg");
+
+    products[1] = new product(
+                "Frozen Hot Chocolate", 
+                "This frozen hot chocolate with whipped cream, chocolate sauce and chocolate curls is the stuff hot summer days are made for!", 
+                "8.99", 
+                "images/frozen-hot-chocolate.jpg");
+
+    products[2] = new product(
+                 "Frozen Sangria", 
+                 "This Frozen Sangria Slush is just five ingredients, comes together in the blender, and will be the hit of the party! Make a double batch!", 
+                 "7.99", 
+                 "images/frozen-sangria.jpg");
+
+    products[3] = new product(
+                "Frozen Lemonade", 
+                "On a hot day, it's hard to beat a tall glass of slushy frozen lemonade! Cool down with this easy drink at your next picnic.", 
+                "7.99", 
+                "images/frozen-lemonade.jpeg");
+
+    products[4] = new product(
+                "Frozen Daiquiri", 
+                "There are two styles of daiquiris: frozen and shaken. This mix is for the frozen version of the basic and original lime Daiquiri. It is simple, delicious, and a fantastic way to cool down on a hot summer day.", 
+                "8.99", 
+                "images/frozen-daiquiri.jpg");
+    products[5] = new product(
+                "Frozen Pina Colada", 
+                "This updated version of a classic summer cocktail includes coconut ice cream and frozen pineapple chunks.", 
+                "9.99", 
+                "images/frozen-pina-colada.jpeg");
+    products[6] = new product("Frozen Mangorita", "Description", "9.99", "url");
+    products[7] = new product("Frozen Chi Chi", "Description", "9.99", "url");
+    products[8] = new product("Frozen Bushwhacker", "Description", "9.99", "url");
     products[9] = new product("Frozen Margarita 9", "Description", "9.99", "url");
     
     
